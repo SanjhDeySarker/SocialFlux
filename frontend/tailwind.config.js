ac4316948bffb726ba1ee98b3db4a9d5e399483b
Sanjh Dey Sarker <sanjhdeysarker@gmail.com>
2025-10-10 16:55:11 +0530

    configuartion-changes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,16 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {} },
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2563eb", // blue-600
+        secondary: "#f9fafb",
+      },
+    },
+  },
   plugins: [],
-};
+}
